23372eb4e26f5751e17fd7555f2e408bfee82b7a
EzequielMora <ezequielmora2006@gmail.com>
2026-07-10 21:19:56 -0300

    Agregar config inicial de Neovim

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+-- Configuración básica de Neovim
+vim.opt.number = true          -- números de línea
+vim.opt.relativenumber = true  -- números relativos (útil para moverte con conteos)
+vim.opt.expandtab = true       -- usar espacios en vez de tabs
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"  -- usar el clipboard del sistema
+
+vim.g.mapleader = " "  -- tecla líder = espacio
